6041508d06bd0a611779ef0b2565cad4898b4c04
Martin Toft <martinolsson@live.no>
2021-12-05 20:58:37 +0100

    Added test for get_countries

--- assignment6/test_webprogramming.py
+++ assignment6/test_webprogramming.py
@@ -1,6 +1,7 @@
 # Import modules to be tested
 #from webvisualization_plots import get_data_from_csv
 from webvisualization_plots import plot_reported_cases_per_million
+from webvisualization_plots import get_countries
 
 def test_plot_reported_cases_per_million():
     """Tests the plot_reported_cases_per_million-function from webvisualization_plots"""
@@ -12,3 +13,8 @@ def test_plot_reported_cases_per_million():
     chart = plot_reported_cases_per_million(start="2020-05-01")
     # With end-date 
     chart = plot_reported_cases_per_million(end="2021-08-01")
+
+def test_get_countries():
+    """Tests the get_countries-function from webvisualization_plots"""
+    countries = get_countries()
+

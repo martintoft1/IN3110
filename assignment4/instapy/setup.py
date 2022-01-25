01d44c7ba0ec48053cfbc456a6c6d744c7199cc5
Martin Toft <martinolsson@live.no>
2022-01-25 15:07:24 +0100

    Change setup to use setuptools for better setup

--- assignment4/instapy/setup.py
+++ assignment4/instapy/setup.py
@@ -1,8 +1,10 @@
-from distutils.core import setup
-
-setup(
+import setuptools
+setuptools.setup(
     name="instapy",
     version="1.0",
-    packages=["instapy"],
-    scripts=["bin/instapy"]
+    author="Martin Toft",
+    packages=setuptools.find_packages(),
+    scripts=["bin/instapy"],
+    setup_requires=["numpy", "setuptools>=18.0"],
+    install_requires=["numpy", "numba", "opencv-python"],
 )

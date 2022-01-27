0186691c926f568cb145616e94f80bba994f1c88
martintoft1 <61971685+martintoft1@users.noreply.github.com>
2022-01-27 13:23:33 +0100

    Delete assignment5 directory

--- assignment5/wikiscrape/setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-setuptools.setup(
-    name="wikiscrape",
-    version="1.0",
-    author="Martin Toft",
-    packages=setuptools.find_packages(),
-    setup_requires=["setuptools>=18.0"],
-)
